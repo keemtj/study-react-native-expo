a25fdc0b67e840cc22e8ce9f5e6c3bbb55ef3df1
keemtj <keemgreat@gmail.com>
2021-10-08 01:01:46 +0900

    :white_check_mark: Add first text to view

---
+++ chapter02/my-first-expo/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>My First React Native Expo</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 30,
+  },
+});
+
+export default App;
